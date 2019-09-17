0dead8eac8ea0b7e138ed919c1de45fb3e665dae
Randall Floyd <rdfloyd@indiana.edu>
2019-09-17 11:02:46 -0400

    Adding step to set ocr_state before save in PagedResourceActor

--- app/actors/hyrax/actors/paged_resource_actor.rb
+++ app/actors/hyrax/actors/paged_resource_actor.rb
@@ -6,6 +6,7 @@ module Hyrax
       def create(env)
         apply_creation_data_to_curation_concern(env)
         apply_save_data_to_curation_concern(env)
+        apply_ocr_option_data_to_curation_concern(env)
         save(env) && next_actor.create(env) && run_callbacks(:after_create_concern, env)
       end
 

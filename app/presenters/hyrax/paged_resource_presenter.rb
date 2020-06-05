94b05b4e0361c9f9dab568636531081176e4d729
Shana Moore <shana.lavina.moore@gmail.com>
2020-06-05 14:05:15 -0700

    wip: add pdf to uv manifest file uv dropdown

--- app/presenters/hyrax/paged_resource_presenter.rb
+++ app/presenters/hyrax/paged_resource_presenter.rb
@@ -10,5 +10,21 @@ module Hyrax
     def holding_location
       HoldingLocationAttributeRenderer.new(solr_document.holding_location).render_dl_row
     end
+
+    # Overrides hyrax
+    # IIIF rendering linking property for inclusion in the manifest
+    #  Called by the `iiif_manifest` gem to add a 'rendering' (eg. a link a download for the resource)
+    #
+    # @return [Array] array of rendering hashes
+    def sequence_rendering
+      renderings = []
+      if solr_document.rendering_ids.present?
+        solr_document.rendering_ids.each do |file_set_id|
+          renderings << manifest_helper.build_rendering(file_set_id)
+          renderings << manifest_helper.build_pdf_rendering(file_set_id)
+        end
+      end
+      renderings.flatten
+    end
   end
 end

1bbc5eb517e2f1df176656a3976033d0e1136dad
Shana Moore <shana.lavina.moore@gmail.com>
2020-06-19 16:56:23 -0700

    add monkey patch to resolve blank html and png captured after spec failures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,7 +19,18 @@ require 'webdrivers'
 require 'i18n/debug' if ENV['I18N_DEBUG']
 require 'byebug' unless ENV['CI']
 require 'noid/rails/rspec'
+# without lines 23-32, screenshots and html captured from failing specs are blank
+# source: https://github.com/mattheworiordan/capybara-screenshot/issues/225
+require "action_dispatch/system_testing/test_helpers/setup_and_teardown" 
+::ActionDispatch::SystemTesting::TestHelpers::SetupAndTeardown.module_eval do
+  def before_setup
+    super
+  end
 
+  def after_teardown
+    super
+  end
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -120,7 +131,7 @@ RSpec.configure do |config|
       Capybara.server_host = '0.0.0.0'
       Capybara.server_port = 3010
       # renaming container because the app domain is taken by google(gTLD)
-      Capybara.app_host = "http://essi_app:3010"
+      Capybara.app_host = 'http://essi_app:3010'
     end
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures

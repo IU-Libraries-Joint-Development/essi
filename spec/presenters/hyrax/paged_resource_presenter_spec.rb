1c30e572c9ca3f2f3b49baeea8fc0d9bad4eee24
Shana Moore <shana.lavina.moore@gmail.com>
2020-06-22 16:48:22 -0700

    add contexts for environment variables

--- spec/presenters/hyrax/paged_resource_presenter_spec.rb
+++ spec/presenters/hyrax/paged_resource_presenter_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Hyrax::PagedResourcePresenter do
   let(:solr_document) { SolrDocument.new(attributes) }
   let(:request) { double(host: 'example.org', base_url: 'http://example.org') }
   let(:user_key) { 'a_user_key' }
-  let(:current_user) { FactoryBot.build(:admin) }
+  let(:current_user) { create :admin }
 
   let(:attributes) do
     { "id" => '888888',
@@ -33,13 +33,39 @@ RSpec.describe Hyrax::PagedResourcePresenter do
       before do
         Hydra::Works::AddFileToFileSet.call(work.file_sets.first,
                                             File.open(fixture_path + '/world.png'), :original_file)
-        allow(current_user).to receive(:admin?).and_return(true)
       end
 
-      it "returns a hash containing the pdf rendering information" do
-        pdf_rendering_hash = {"@id"=>"/concern/paged_resources/#{work.id}/pdf", "label"=>"Download as PDF", "format"=>"application/pdf"}
-        expect(subject).to be_an Array
-        expect(subject).to include(pdf_rendering_hash)
+      context 'when allow_pdf_download config is true' do
+        before do
+          ESSI.config[:essi][:allow_pdf_download] = true
+        end
+        context 'when user is an admin' do
+          before do
+            allow(current_user).to receive(:admin?).and_return(true)
+          end
+
+          it 'returns a hash containing the pdf rendering information' do
+            pdf_rendering_hash = {"@id"=>"/concern/paged_resources/#{work.id}/pdf", "label"=>"Download as PDF", "format"=>"application/pdf"}
+            expect(subject).to be_an Array
+            expect(subject).to include(pdf_rendering_hash)
+          end
+        end
+
+        context 'when user is not an admin' do
+          before do
+            allow(current_user).to receive(:admin?).and_return(false)
+          end
+
+          it 'returns a hash containing the pdf rendering information' do
+            pdf_rendering_hash = {"@id"=>"/concern/paged_resources/#{work.id}/pdf", "label"=>"Download as PDF", "format"=>"application/pdf"}
+            expect(subject).to be_an Array
+            expect(subject).not_to include(pdf_rendering_hash)
+          end
+        end
+      end
+
+      context 'when allow_pdf_download config is false' do
+
       end
     end
   end

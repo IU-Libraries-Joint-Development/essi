530aaf51666e13f2f4ae89867339ba6b8545794a
Shana Moore <shana.lavina.moore@gmail.com>
2020-06-23 14:54:33 -0700

    wip: feature spec

--- spec/features/download_paged_resource_pdf_spec.rb
+++ spec/features/download_paged_resource_pdf_spec.rb
@@ -11,24 +11,9 @@ RSpec.feature 'Download a PagedResource PDF', type: :system, js: true do
       FactoryBot.create :admin
     end
     let(:paged_resource) { create :paged_resource_with_one_image, user: user }
-    let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
-    let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(source_id: admin_set_id) }
-    let(:workflow) { Sipity::Workflow.create!(active: true, name: 'test-workflow', permission_template: permission_template) }
 
     before do
-      # Create a single action that can be taken
-      Sipity::WorkflowAction.create!(name: 'submit', workflow: workflow)
-
-      # Grant the user access to deposit into the admin set.
-      Hyrax::PermissionTemplateAccess.create!(
-        permission_template_id: permission_template.id,
-        agent_type: 'user',
-        agent_id: user.user_key,
-        access: 'deposit'
-      )
       ESSI.config[:essi][:allow_pdf_download] = true
-      # Ensure empty requirement for ldap group authorization
-      allow(ESSI.config[:authorized_ldap_groups]).to receive(:blank?).and_return(true)
       login_as user
     end
 
@@ -36,15 +21,16 @@ RSpec.feature 'Download a PagedResource PDF', type: :system, js: true do
       page.has_css?('button.download')
     end
 
-    scenario do
-      visit polymorphic_path [paged_resource]
+    #scenario do
+    #  visit polymorphic_path [paged_resource]
 
-      wait_until_uv_loads
-      find('.download').click
-      choose('Download as PDF (pdf)')
-      click('Download')
-      expect(page).to have_current_path("#{polymorphic_path [paged_resource]}/pdf")
+    #  wait_until_uv_loads
+    #  find('.download').click
+    #  choose('Download as PDF (pdf)')
+    #  click('Download')
+    #  expect(page).to have_current_path("#{polymorphic_path [paged_resource]}/pdf")
 
-    end
+    #  #TODO: Add Coverpage Specs 
+    #end
   end
 end

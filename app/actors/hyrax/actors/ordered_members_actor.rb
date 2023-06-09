a27fd1bd35a32e5364387cce373b15a84a4fbbfa
Daniel Pierce <dlpierce@indiana.edu>
2023-06-09 16:56:46 -0400

    Use concat when attaching ordered members
    The prior method of assigning an array of the new ordered members resulted in new hasPart
    identifiers and list_source bloat from the old member resources not being cleaned up.

--- app/actors/hyrax/actors/ordered_members_actor.rb
+++ app/actors/hyrax/actors/ordered_members_actor.rb
@@ -16,7 +16,10 @@ module Hyrax
       # @param [ActiveFedora::Base] work the parent work
       def attach_ordered_members_to_work(work)
         acquire_lock_for(work.id) do
-          work.ordered_members = ordered_members
+          # Modified to avoid recreating hasPart entries in the list_source with new identifiers.
+          # The old resources were not cleaned up, leading to poor list_source performance.
+          new_members = ordered_members - work.ordered_members
+          work.ordered_members.concat(new_members)
           work.save
           ordered_members.each do |file_set|
             Hyrax.config.callback.run(:after_create_fileset, file_set, user)

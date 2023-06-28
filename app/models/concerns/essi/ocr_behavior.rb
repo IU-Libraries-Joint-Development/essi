4570c4089ad70c0e80cf0663ddf2a4e677bfd448
Adam Ploshay <aploshay@iu.edu>
2023-06-28 17:37:06 -0400

    attempt to load missing ocr_state property during indexing

--- app/models/concerns/essi/ocr_behavior.rb
+++ app/models/concerns/essi/ocr_behavior.rb
@@ -3,7 +3,20 @@ module ESSI
     extend ActiveSupport::Concern
 
     def ocr_searchable?
-      self.ocr_state == 'searchable'
+      load_ocr_state
+      self.try(:ocr_state) == 'searchable' # handle missing ocr_state property via try
+    end
+
+    def load_ocr_state
+      unless self.respond_to?(:ocr_state)
+        Rails.logger.warn "ocr_state property missing from #{self.class.to_s} item #{self.title} (#{self.id})"
+        self.class.new # attempt loading unloaded flexible metadata profile
+        if self.respond_to?(:ocr_state)
+          Rails.logger.info "ocr_state property successfully loaded"
+        else
+          Rails.logger.warn "ocr_state property failed to load"
+        end
+      end
     end
 
     def to_solr(solr_doc = {})

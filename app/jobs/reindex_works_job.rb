abb1f2d559d3e63d931d55ea2b2bb5292ea00c86
Randall Floyd <rdfloyd@indiana.edu>
2023-09-25 17:18:08 -0400

    Parametrize the nested indexer depth for control during reindexing

--- app/jobs/reindex_works_job.rb
+++ app/jobs/reindex_works_job.rb
@@ -6,7 +6,13 @@ class ReindexWorksJob < ApplicationJob
     Hyrax.config.registered_curation_concern_types.each do |work_type|
       work_type.constantize.find_each do |work|
         Rails.logger.info "Reindexing #{work.class}: #{work.id}"
-        work.update_index
+        begin
+          work.update_index
+        rescue Samvera::NestingIndexer::Exceptions::ExceededMaximumNestingDepthError
+          Rails.logger.info "ReindexWorksJob: Children of #{work.class} #{work.id} not indexed; exceeds nested depth maximum."
+        rescue => error
+          Rails.logger.error "ReindexWorksJob: Reindexing #{work.id} failed,  #{error.message}"
+        end
       end
     end
   end

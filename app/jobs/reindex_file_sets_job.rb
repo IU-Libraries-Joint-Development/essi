56ae7dde32e480d75790e2b3d991e70877d784a7
Randall Floyd <rdfloyd@indiana.edu>
2023-09-29 14:06:55 -0400

    Refactoring reindexing jobs to use Solr queries and track document index status

--- app/jobs/reindex_file_sets_job.rb
+++ app/jobs/reindex_file_sets_job.rb
@@ -1,11 +1,45 @@
 # frozen_string_literal: true
 
-# Triggers reindexing on all FileSets
 class ReindexFileSetsJob < ApplicationJob
-  def perform
-    FileSet.find_each do |fs|
-      Rails.logger.info "Reindexing #{fs.class}: #{fs.id}"
-      fs.update_index
+  def perform(file_set_ids = [], opts = {})
+    index_query_field = opts.dig(:index_query_field) || 'iiif_index_strategy_tesim'
+    index_query_value = opts.dig(:index_query_value) || IndexerHelper.iiif_index_strategy
+    row_count = opts.dig(:row_count) || 500
+    sleep_break = opts.dig(:row_count) || 10
+    if file_set_ids.empty?
+      index_query = "-#{index_query_field}:#{index_query_value}"
+      model_query = "has_model_ssim:FileSet"
+      loop do
+        results = ActiveFedora::SolrService.instance.conn.get(
+          ActiveFedora::SolrService.select_path,
+          params: { qt: "search", q: [model_query], fq: [index_query], fl: "id", rows: row_count}
+        )
+
+        break if results["response"]["numFound"] == 0
+
+        results["response"]["docs"].each do |solr_hit|
+          index_file_set(solr_hit["id"])
+        end
+        # Give the backend and the jobs a break...
+        sleep sleep_break
+      end
+    else
+      file_set_ids.each do |id|
+        index_file_set(id)
+      end
     end
   end
+
+  private
+
+    def index_file_set(id)
+      begin
+        fs = FileSet.find(id)
+        FileSet.new
+        Rails.logger.info "Reindexing #{fs.class}: #{fs.id}"
+        # fs.update_index
+      rescue => error
+        Rails.logger.error "ReindexWorksJob: Reindexing #{work.id} failed,  #{error.message}"
+      end
+    end
 end
--- app/jobs/reindex_works_job.rb
+++ app/jobs/reindex_works_job.rb
@@ -1,18 +1,41 @@
 # frozen_string_literal: true
 
-# Triggers reindexing on all works
 class ReindexWorksJob < ApplicationJob
-  def perform
+
+  def perform(opts = {})
+    index_query_field = opts.dig(:index_query_field) || 'iiif_index_strategy_tesim'
+    index_query_value = opts.dig(:index_query_value) || IndexerHelper.iiif_index_strategy
+    index_file_sets = opts.dig(:index_file_sets) || true
+    async_fs_jobs = opts.dig(:async_fs_jobs) || false
+    row_count = opts.dig(:row_count) || 100
+    sleep_break = opts.dig(:row_count) || 10
     Hyrax.config.registered_curation_concern_types.each do |work_type|
-      work_type.constantize.find_each do |work|
-        Rails.logger.info "Reindexing #{work.class}: #{work.id}"
-        begin
-          work.update_index
-        rescue Samvera::NestingIndexer::Exceptions::ExceededMaximumNestingDepthError
-          Rails.logger.info "ReindexWorksJob: Children of #{work.class} #{work.id} not indexed; exceeds nested depth maximum."
-        rescue => error
-          Rails.logger.error "ReindexWorksJob: Reindexing #{work.id} failed,  #{error.message}"
+      index_query = "-#{index_query_field}:#{index_query_value}"
+      model_query = "has_model_ssim:#{work_type.constantize}"
+      loop do
+        results = ActiveFedora::SolrService.instance.conn.get(
+          ActiveFedora::SolrService.select_path,
+          params: { qt: "search", q: [model_query], fq: [index_query], fl: "id,file_set_ids_ssim", rows: row_count}
+        )
+
+        break if results["response"]["numFound"] == 0
+
+        results["response"]["docs"].each do |solr_hit|
+          work = work_type.constantize.find(solr_hit["id"])
+          work_type.constantize.class.new
+          begin
+            work.update_index
+          rescue Samvera::NestingIndexer::Exceptions::ExceededMaximumNestingDepthError
+            Rails.logger.info "ReindexWorksJob: Children of #{work.class} #{work.id} not indexed; exceeds nested depth maximum."
+          rescue => error
+            Rails.logger.error "ReindexWorksJob: Reindexing #{work.id} failed,  #{error.message}"
+          end
+          perform_method = async_fs_jobs ? 'perform_later' : 'perform_now'
+          ReindexFileSetsJob.send(perform_method.to_sym, (solr_hit["file_set_ids_ssim"])) if index_file_sets
         end
+
+        # Give the backend and the jobs a break...
+        sleep sleep_break
       end
     end
   end

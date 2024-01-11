b26dd0ecea52f50adf75febe9fa412a92510ec17
Adam Ploshay <aploshay@iu.edu>
2024-01-11 10:33:15 -0500

    [ESSI-875] bulkrax extensions: update dynamic schema patch for 5.x

--- lib/extensions/bulkrax/object_factory/create_with_dynamic_schema.rb
+++ lib/extensions/bulkrax/object_factory/create_with_dynamic_schema.rb
@@ -2,10 +2,12 @@ module Extensions
   module Bulkrax
     module ObjectFactory
       module CreateWithDynamicSchema
-        # unmodified from bulkrax 5.x
+        # modified from bulkrax 5.x to apply a supplied dynamic_schema_id to initial object build
         def create
           attrs = transform_attributes
-          @object = klass.new
+          init_attrs = {}
+          init_attrs = { dynamic_schema_id: attrs[:dynamic_schema_id] } if attrs[:dynamic_schema_id].present? && klass.new.respond_to?(:dynamic_schema_id)
+          @object = klass.new(init_attrs)
           object.reindex_extent = Hyrax::Adapters::NestingIndexAdapter::LIMITED_REINDEX if defined?(Hyrax::Adapters::NestingIndexAdapter) && object.respond_to?(:reindex_extent)
           run_callbacks :save do
             run_callbacks :create do

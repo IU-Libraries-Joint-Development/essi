c83c472c292b9914bb0fe677b2535203ee704724
Adam Ploshay <aploshay@iu.edu>
2024-03-15 10:43:14 -0400

    [ESSI-1949] prepare monkeypatch for #add_new_work_ids_not_already_in_curation_concern

---
+++ lib/extensions/hyrax/actors/apply_order_actor/add_members_single_save.rb
@@ -0,0 +1,22 @@
+# unmodified from hyrax
+module Extensions
+  module Hyrax
+    module Actors
+      module ApplyOrderActor
+        module AddMembersSingleSave
+          def add_new_work_ids_not_already_in_curation_concern(env, ordered_member_ids)
+            (ordered_member_ids - env.curation_concern.ordered_member_ids).each do |work_id|
+              work = ::ActiveFedora::Base.find(work_id)
+              if can_edit_both_works?(env, work)
+                env.curation_concern.ordered_members << work
+                env.curation_concern.save!
+              else
+                env.curation_concern.errors[:ordered_member_ids] << "Works can only be related to each other if user has ability to edit both."
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end

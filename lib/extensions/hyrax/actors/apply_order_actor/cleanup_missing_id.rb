3aa816b2d7099efa53e2dd855feb31ff655a30cc
Adam Ploshay <aploshay@iu.edu>
2024-03-15 10:50:38 -0400

    [ESSI-1949] prep monkeypatch to #cleanup_ids_to_remove_from_curation_concern

---
+++ lib/extensions/hyrax/actors/apply_order_actor/cleanup_missing_id.rb
@@ -0,0 +1,18 @@
+# unmodified from hyrax
+module Extensions
+  module Hyrax
+    module Actors
+      module ApplyOrderActor
+        module CleanupMissingId
+          def cleanup_ids_to_remove_from_curation_concern(curation_concern, ordered_member_ids)
+            (curation_concern.ordered_member_ids - ordered_member_ids).each do |old_id|
+              work = ::ActiveFedora::Base.find(old_id)
+              curation_concern.ordered_members.delete(work)
+              curation_concern.members.delete(work)
+            end
+          end
+        end
+      end
+    end
+  end
+end

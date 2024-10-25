ec5a96580a4ad43528c58e7bcb8948ed0c6526ad
Adam Ploshay <aploshay@iu.edu>
2024-10-25 14:06:06 -0400

    [ESSI-2025] add, use #link_to_campus_facet

--- app/helpers/facet_helper.rb
+++ app/helpers/facet_helper.rb
@@ -3,12 +3,13 @@ module FacetHelper
     CampusService.find(value)[:term] || ''
   end
 
-  # imported from hyrax to prep campus-specific version
+  # campus-specific version of link_to_facet
   # @param item [Object]
   # @param field [String]
   # @return [ActiveSupport::SafeBuffer] the html_safe link
-  def link_to_facet(item, field)
+  def link_to_campus_facet(item, field)
     path = main_app.search_catalog_path(search_state.add_facet_params_and_redirect(field, item))
+    item = campus_label(item)
     link_to(item, path)
   end
 end

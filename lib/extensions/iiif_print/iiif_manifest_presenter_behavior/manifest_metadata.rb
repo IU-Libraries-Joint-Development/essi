9b00fcaaec59726dcc776f26241236bff95041c9
Adam Ploshay <aploshay@iu.edu>
2024-11-12 20:52:56 -0500

    [ESSI-2027] monkeypatch iiif_print for manifest metadata

--- lib/extensions/iiif_print/iiif_manifest_presenter_behavior/manifest_metadata.rb
+++ lib/extensions/iiif_print/iiif_manifest_presenter_behavior/manifest_metadata.rb
@@ -4,10 +4,10 @@ module Extensions
       module ManifestMetadata
         def self.included(base)
           base.class_eval do
-            # unmodified from iiif_print
-            def manifest_metadata
+            # modified from iiif_print to optionally receive fields
+            def manifest_metadata(fields: ::IiifPrint.default_metadata_fields)
               # ensure we are using a SolrDocument
-              @manifest_metadata ||= IiifPrint.manifest_metadata_from(work: model.solr_document, presenter: self)
+              @manifest_metadata ||= ::IiifPrint.manifest_metadata_from(work: model.solr_document, presenter: self, fields: fields)
             end
           end
         end
--- lib/extensions/iiif_print/manifest_metadata.rb
+++ lib/extensions/iiif_print/manifest_metadata.rb
@@ -3,11 +3,15 @@ module Extensions
     module ManifestMetadata
       def self.included(base)
         base.class_eval do
-          # unmodified from iiif_print
-          def self.manifest_metadata_from(work:, presenter:)
+          # modified from iiif_print to receive fields
+          def self.manifest_metadata_from(work:, presenter:, fields: default_metadata_fields)
             current_ability = presenter.try(:ability) || presenter.try(:current_ability)
             base_url = presenter.try(:base_url) || presenter.try(:request)&.base_url
-            ::IiifPrint.manifest_metadata_for(work: work, current_ability: current_ability, base_url: base_url)
+            ::IiifPrint.manifest_metadata_for(work: work, current_ability: current_ability, base_url: base_url, fields: fields)
+          end
+
+          def self.default_metadata_fields
+            defined?(::AllinsonFlex) ? fields_for_allinson_flex : default_fields
           end
         end
       end

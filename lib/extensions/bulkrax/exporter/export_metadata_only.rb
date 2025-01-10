39626a8ad14e0ebbf3805abd2ccc50d51791f44b
Adam Ploshay <aploshay@iu.edu>
2025-01-10 15:14:32 -0500

    [ESSI-857] [ESSI-2050] monkeypatch to disable files export

--- lib/extensions/bulkrax/exporter/export_metadata_only.rb
+++ lib/extensions/bulkrax/exporter/export_metadata_only.rb
@@ -1,17 +1,19 @@
-# unmodified from bulkrax 5.5.1
+# modified from bulkrax 5.5.1
 module Extensions
   module Bulkrax
     module Exporter
       module ExportMetadataOnly
+        # modified to remove option for files export
         def export_type_list
           [
             [I18n.t('bulkrax.exporter.labels.metadata'), 'metadata'],
-            [I18n.t('bulkrax.exporter.labels.full'), 'full']
+            # [I18n.t('bulkrax.exporter.labels.full'), 'full']
           ]
         end
 
+        # modified to never support file export
         def metadata_only?
-          export_type == 'metadata'
+          true # export_type == 'metadata'
         end
       end
     end

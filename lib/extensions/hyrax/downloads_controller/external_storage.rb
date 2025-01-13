1a455b4c4cbea5ba48036b679673aba46d95cbfb
Adam Ploshay <aploshay@iu.edu>
2025-01-13 13:11:20 -0500

    fix for downloading extracted_text in external_storage case for original_file

--- lib/extensions/hyrax/downloads_controller/external_storage.rb
+++ lib/extensions/hyrax/downloads_controller/external_storage.rb
@@ -4,7 +4,9 @@ module Extensions
     module DownloadsController
       module ExternalStorage
         def show
-          if asset.content_location&.start_with?('s3://')
+          if params[:file] && params[:file] == 'extracted_text'
+            super
+          elsif asset.content_location&.start_with?('s3://')
             ext_id = asset.content_location.split('/').last
             external_asset = ESSI.external_storage.get(ext_id)
             send_data external_asset.body.read, filename: ext_id

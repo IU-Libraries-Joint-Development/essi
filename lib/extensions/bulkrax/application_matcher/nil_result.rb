76b03cd38ef198a6b2731faf156430e291cd7b93
Adam Ploshay <aploshay@iu.edu>
2025-01-22 10:32:51 -0500

    [ESSI-875] [ESSI-2052] apply monkeypatch for correctly importing nil single values

--- lib/extensions/bulkrax/application_matcher/nil_result.rb
+++ lib/extensions/bulkrax/application_matcher/nil_result.rb
@@ -2,7 +2,7 @@ module Extensions
   module Bulkrax
     module ApplicationMatcher
       module NilResult
-        # unmodified from bulkrax 5.5.1
+        # modified from bulkrax 5.5.1
         def result(_parser, content)
           return nil if self.excluded == true || ::Bulkrax.reserved_properties.include?(self.to)
           return nil if self.if && (!self.if.is_a?(::Array) && self.if.length != 2)
@@ -14,6 +14,8 @@ module Extensions
           # @result will evaluate to an empty string for nil content values
           @result = content.to_s.gsub(/\s/, ' ').strip # remove any line feeds and tabs
           # blank needs to be based to split, only skip nil
+          # below line added to recapture lost nil values, see bulkrax issue #1010
+          @result = nil if @result.blank?
           process_split unless @result.nil?
           @result = @result[0] if @result.is_a?(::Array) && @result.size == 1
           process_parse

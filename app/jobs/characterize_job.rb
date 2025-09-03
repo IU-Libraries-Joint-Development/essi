e03fa1437cecddfee9ae5a8ce54377ff9aa9b2ea
Adam Ploshay <aploshay@iu.edu>
2025-09-03 12:06:35 -0400

    [ESSI-2130] add more #find_or_retrieve methods, refactor calls

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -10,16 +10,9 @@ class CharacterizeJob < ApplicationJob
   def perform(file_set, file_id, filepath = nil, derivation_path = nil, delete_characterization_path = false)
     raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     unless filepath && File.exist?(filepath)
-      if file_set.content_location&.start_with?('s3://') # Ensure external file is available locally
-        ext_id = file_set.content_location.split('/').last
-        ext_resp = ESSI.external_storage.get(ext_id)
-        filepath = Hyrax::WorkingDirectory.send(:copy_stream_to_working_directory, ext_id, ext_id, ext_resp.body)
-        delete_characterization_path = true
-      else
-        filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id)
-        delete_characterization_path = false
-      end
+      delete_characterization_path = file_set.external?
     end
+    filepath = file_set.find_or_retrieve(file_id: file_id, filepath: filepath)
     Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
     Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
     file_set.characterization_proxy.save!
--- app/jobs/create_ocr_job.rb
+++ app/jobs/create_ocr_job.rb
@@ -7,15 +7,7 @@ class CreateOCRJob < CreateDerivativesJob
   def perform(file_set, file_id, filepath = nil)
     return if file_set.video? && !Hyrax.config.enable_ffmpeg
 
-    unless filepath && File.exist?(filepath)
-      if file_set.content_location&.start_with?('s3://') # Ensure external file is available locally
-        ext_id = file_set.content_location.split('/').last
-        ext_resp = ESSI.external_storage.get(ext_id)
-        filepath = Hyrax::WorkingDirectory.send(:copy_stream_to_working_directory, ext_id, ext_id, ext_resp.body)
-      else
-        filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id)
-      end
-    end
+    filepath = file_set.find_or_retrieve(file_id: file_id, filepath: filepath)
 
     # using #create_derivatives instead of #create_ocr_derivatives to use IIIF Print
     # @see https://github.com/scientist-softserv/iiif_print/blob/d14246664048c708071c7ff4de2e9a34aa703465/app/services/iiif_print/pluggable_derivative_service.rb#L25

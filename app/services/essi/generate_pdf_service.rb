b0b2b48a1eaf466f14a9b4da3855e2006457f02c
Adam Ploshay <aploshay@iu.edu>
2025-09-03 11:17:55 -0400

    [ESSI-2130] ensure #find_or_retrieve calls handle fedora/s3 storage

--- app/services/essi/generate_pdf_service.rb
+++ app/services/essi/generate_pdf_service.rb
@@ -73,7 +73,15 @@ module ESSI
     def generate_width(file_set_id)
       begin
         file_set = FileSet.find(file_set_id)
-        filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_set.original_file.id, file_set.id)
+
+        if file_set.content_location&.start_with?('s3://') # Ensure external file is available locally
+          ext_id = file_set.content_location.split('/').last
+          ext_resp = ESSI.external_storage.get(ext_id)
+          filepath = Hyrax::WorkingDirectory.send(:copy_stream_to_working_directory, ext_id, ext_id, ext_resp.body)
+        else
+          filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_set.original_file.id, file_set.id)
+        end
+
         terms = Hydra::Works::CharacterizationService.run(file_set.original_file, filepath)
         CharacterizeJob.perform_later(file_set, file_set.original_file.id)
       rescue

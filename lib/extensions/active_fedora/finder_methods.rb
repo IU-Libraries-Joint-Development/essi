f37b770acb56ff9d6192c270f578c3ef8140371c
Daniel Pierce <dlpierce@iu.edu>
2026-03-06 16:47:06 -0500

    Original search_in_batches

---
+++ lib/extensions/active_fedora/finder_methods.rb
@@ -0,0 +1,24 @@
+module Extensions
+  module ActiveFedora
+    module FinderMethods
+      def search_in_batches(conditions, opts = {})
+        opts[:q] = create_query(conditions)
+        opts[:qt] = @klass.solr_query_handler
+        # set default sort to created date ascending
+        opts[:sort] = @klass.default_sort_params unless opts[:sort].present?
+
+        batch_size = opts.delete(:batch_size) || 1000
+        select_path = ActiveFedora::SolrService.select_path
+
+        counter = 0
+        loop do
+          counter += 1
+          response = ActiveFedora::SolrService.instance.conn.paginate counter, batch_size, select_path, params: opts
+          docs = response["response"]["docs"]
+          yield docs
+          break unless docs.has_next?
+        end
+      end
+    end
+  end
+end

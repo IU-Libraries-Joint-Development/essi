6261d78022b45901a019b6f8c87ea9a5ed200a7b
Daniel Pierce <dlpierce@iu.edu>
2026-03-06 16:53:34 -0500

    search_in_batches using POST

--- lib/extensions/active_fedora/finder_methods.rb
+++ lib/extensions/active_fedora/finder_methods.rb
@@ -6,14 +6,16 @@ module Extensions
         opts[:qt] = @klass.solr_query_handler
         # set default sort to created date ascending
         opts[:sort] = @klass.default_sort_params unless opts[:sort].present?
+        opts[:method] ||= :post
 
         batch_size = opts.delete(:batch_size) || 1000
-        select_path = ActiveFedora::SolrService.select_path
+        select_path = ::ActiveFedora::SolrService.select_path
 
         counter = 0
         loop do
           counter += 1
-          response = ActiveFedora::SolrService.instance.conn.paginate counter, batch_size, select_path, params: opts
+          response = ::ActiveFedora::SolrService.instance.conn.paginate counter, batch_size, select_path,
+                                                                      { params: opts, method: opts[:method] }
           docs = response["response"]["docs"]
           yield docs
           break unless docs.has_next?

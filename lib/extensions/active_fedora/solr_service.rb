2887e63bc23800282ab8569591451a3450a0c91b
Daniel Pierce <dlpierce@iu.edu>
2026-03-10 15:19:50 -0400

    These are class methods we're trying to prepend.

--- lib/extensions/active_fedora/solr_service.rb
+++ lib/extensions/active_fedora/solr_service.rb
@@ -2,28 +2,34 @@
 module Extensions
   module ActiveFedora
     module SolrService
-      # Modified from active_fedora 12.2.4 solr_service.rb
-      # to use POST by default
-      def query(query, args = {})
-        Base.logger.warn "Calling ActiveFedora::SolrService.get without passing an explicit value for ':rows' is not recommended. You will end up with Solr's default (usually set to 10)\nCalled by #{caller[0]}" unless args.key?(:rows)
-        method = args.delete(:method) || :post
-
-        result = case method
-                 when :get
-                   get(query, args)
-                 when :post
-                   post(query, args)
-                 else
-                   raise "Unsupported HTTP method for querying SolrService (#{method.inspect})"
-                 end
-        result['response']['docs'].map do |doc|
-          ActiveFedora::SolrHit.new(doc)
-        end
+      def self.prepended(mod)
+        mod.singleton_class.prepend(ClassMethods)
       end
 
-      def count(query, args = {})
-        args = args.merge(rows: 0)
-        SolrService.post(query, args)['response']['numFound'].to_i
+      module ClassMethods
+        # Modified from active_fedora 12.2.4 solr_service.rb
+        # to use POST by default
+        def query(query, args = {})
+          ::ActiveFedora::Base.logger.warn "Calling ActiveFedora::SolrService.get without passing an explicit value for ':rows' is not recommended. You will end up with Solr's default (usually set to 10)\nCalled by #{caller[0]}" unless args.key?(:rows)
+          method = args.delete(:method) || :post
+
+          result = case method
+                   when :get
+                     get(query, args)
+                   when :post
+                     post(query, args)
+                   else
+                     raise "Unsupported HTTP method for querying SolrService (#{method.inspect})"
+                   end
+          result['response']['docs'].map do |doc|
+            ::ActiveFedora::SolrHit.new(doc)
+          end
+        end
+
+        def count(query, args = {})
+          args = args.merge(rows: 0)
+          ::ActiveFedora::SolrService.post(query, args)['response']['numFound'].to_i
+        end
       end
     end
   end

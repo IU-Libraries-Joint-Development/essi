45e46f10e6e969287d5dd5d65996332f7f44a6b6
Daniel Pierce <dlpierce@iu.edu>
2026-04-09 11:50:27 -0400

    Fix top level lookup in extensions

--- lib/extensions/allinson_flex/optimized_admin_set_lookup.rb
+++ lib/extensions/allinson_flex/optimized_admin_set_lookup.rb
@@ -1,11 +1,13 @@
 module Extensions
   module AllinsonFlex
     module OptimizedAdminSetLookup
+      private
+
       # Modified from AllinsonFlex::DynamicSchemaService to avoid fedora request
       def context_for(admin_set_id:)
-        cxt = AllinsonFlex::Context.find_metadata_context_for(admin_set_id: admin_set_id)
+        cxt = ::AllinsonFlex::Context.find_metadata_context_for(admin_set_id: admin_set_id)
         if cxt.blank?
-          raise AllinsonFlex::NoAllinsonFlexContextError.new(
+          raise ::AllinsonFlex::NoAllinsonFlexContextError.new(
             "No Metadata Context for Admin Set #{admin_set_id}"
           )
         end

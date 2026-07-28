66afd9079ed4e348e2708a06f43b6973fba06691
Chris Colvard <chris.colvard@gmail.com>
2026-07-28 14:15:04 -0400

    Only render per_page widget if logged in and enforce per_page limit by changing blacklight config in before action

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -4,6 +4,7 @@ class CatalogController < ApplicationController
   include ESSI::DynamicCatalogBehavior
   # This filter applies the hydra access controls
   before_action :enforce_show_permissions, only: :show
+  before_action :limit_per_page, only: :index
 
   def self.uploaded_field
     solr_name('system_create', :stored_sortable, type: :date)
@@ -19,6 +20,8 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     config.http_method = :post
+
+    config.add_results_collection_tool(:per_page_widget, if: :current_user)
 
     # IiifPrint index fields
     config.add_index_field 'all_text_tsimv', highlight: true, helper_method: :render_ocr_snippets
@@ -355,5 +358,11 @@ class CatalogController < ApplicationController
   # this method is not called in that context.
   def render_bookmarks_control?
     false
+  end
+
+  private
+
+  def limit_per_page
+    blacklight_config.max_per_page = 10 unless current_user.present?
   end
 end
